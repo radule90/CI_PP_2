f3ab8c2a7a2f2572b0d371f8af248626090368a8
radule90 <milosradulovic90@hotmail.com>
2023-02-13 15:32:41 +0000

    Add code to generate random computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,5 +20,28 @@ for (let button of buttons) {
         button.addEventListener("click", function() {
         const userChoice = this.getAttribute("data-selection");
         userChoiceDisplay.innerHTML = userChoice;
+        randomComputerChoice();
     })
+}
+
+/**
+ * Generates computer random choice and associate the name to number
+ */
+
+function randomComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * buttons.length + 1);
+    let computerChoiceName;
+    switch(randomNumber) {
+        case 1:
+            computerChoiceName = "Rock";
+            break;
+        case 2:
+            computerChoiceName = "Paper";
+            break;
+        case 3:
+            computerChoiceName = "Scissors";
+            break;
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoiceName;
 }
