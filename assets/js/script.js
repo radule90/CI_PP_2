80d9d6a9c28833b7990291cf6bb447e4c7f30ab7
radule90 <milosradulovic90@hotmail.com>
2023-02-13 15:00:55 +0000

    Declare variables for DOM elements

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Declare variables for DOM elements
+ */
+
+const buttons = document.querySelectorAll("[data-selection]");
+const userChoiceDisplay = document.getElementById("user-choice-display");
+const computerChoiceDisplay = document.getElementById("computer-choice-display");
+const resultDisplay = getElementById("result-display");
+const userScore = getElementById("user-score");
+const draw = getElementById("draw");
+const computerScore = getElementById("computer-score");
