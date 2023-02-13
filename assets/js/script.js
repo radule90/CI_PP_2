10e11961f4d356138329852b49fcb37565d70765
radule90 <milosradulovic90@hotmail.com>
2023-02-13 15:15:15 +0000

    Add event listener to buttons and fix variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,20 @@
 const buttons = document.querySelectorAll("[data-selection]");
 const userChoiceDisplay = document.getElementById("user-choice-display");
 const computerChoiceDisplay = document.getElementById("computer-choice-display");
-const resultDisplay = getElementById("result-display");
-const userScore = getElementById("user-score");
-const draw = getElementById("draw");
-const computerScore = getElementById("computer-score");
+const resultDisplay = document.getElementById("result-display");
+const userScore = document.getElementById("user-score");
+const draw = document.getElementById("draw");
+const computerScore = document.getElementById("computer-score");
+
+
+
+/**
+ * Add event listener to all the buttons
+ */
+
+for (let button of buttons) {
+        button.addEventListener("click", function() {
+        const userChoice = this.getAttribute("data-selection");
+        userChoiceDisplay.innerHTML = userChoice;
+    })
+}
